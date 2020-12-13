3288e1a9cf13f8aabab270e735e940341137b786
saumilshah66 <shah.saumil66@gmail.com>
2020-12-13 05:33:47 -0500

    Data transforms added

--- training.py
+++ training.py
@@ -6,12 +6,13 @@ from PIL import Image
 import random
 import matplotlib.pyplot as plt
 import cv2
-from torchvision import transforms
+import torchvision.transforms as transforms
 from skimage import io, transform
 import _pickle as pickle
 import torch.nn as nn
 import torchvision.models as models
 import torch.nn.functional as F
+import torchvision.models as models
 
 class SegmentationData(data.Dataset):
     def __init__(self, file_name="Data.csv"):
@@ -73,25 +74,56 @@ class ToTensor(object):
         return {'image': torch.from_numpy(image),
                 'id': torch.from_numpy(id_)}
 
+class RandomFlip(object):
+    def __call__(self, sample):
+        if random.randint(0,1)==1:
+            sample['image'] = cv2.flip(sample['image'], 1)
+        return sample
+
+class Normaliza(object):
+    def __init__(self, mean, std):
+        self.mean = mean
+        self.std = std
+    def __call__(self, sample):
+        img = sample['image']
+        img -= mean
+        img /= std
+        sample['image'] = img
+        return sample
+
+class Trans(object):
+    def __init__(self, mean, std):
+        self.norm = Normaliza(mean, std)
+        self.flip = RandomFlip()
+        self.tensor = ToTensor()
+    
+    def __call__(self, sample):
+        return self.tensor(self.norm(self.flip(sample)))
+
 # Model definition
 class SegmentationNN(nn.Module):
     def __init__(self):
+        resnet18 = models.resnet18(pretrained=True)
         pass
         
     def forward(self):
         pass
 
-
 if __name__=="__main__":
-    scale = Rescale(32)
-# crop = RandomCrop(128)
-# composed = transforms.Compose([Rescale(256),
-#                                RandomCrop(224)])
+    mean=[0.485, 0.456, 0.406]
+    std=[0.229, 0.224, 0.225]
 
+    data_transform = Trans(np.array(mean), np.array(std))           
+    
+    scale = Rescale(32)
     dataloader = SegmentationData(file_name="Data.csv")
+    
     fig = plt.figure()
     for i in range(4):
         sample = scale(dataloader[i])
+        new = {'image':sample['image'].copy(), 'id':sample['id'].copy()}
+        final = data_transform(new)
+        
         print(i, sample['image'].shape, np.argmax(sample['id']))
         ax = plt.subplot(1, 4, i + 1)
         plt.tight_layout()
@@ -101,3 +133,5 @@ if __name__=="__main__":
         if i == 3:
             plt.show()
             break
+        
+        

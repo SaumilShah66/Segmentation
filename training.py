6cfb428e943e65476d08006cb0b111fa23981029
saumilshah66 <shah.saumil66@gmail.com>
2020-12-14 03:07:55 -0500

    Cuda device

--- training.py
+++ training.py
@@ -127,6 +127,8 @@ def give_batch(batch_size, batch_number, dataloader, scale, data_transform):
 
 def train(dataloader, scale, data_transform, batch_size):
     vgg = give_vgg()
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    vgg.to(device)
     criterion = nn.L1Loss()
     optimizer = torch.optim.Adam(vgg.parameters(), lr=0.001)
     total_batches = len(dataloader)//batch_size
@@ -134,6 +136,8 @@ def train(dataloader, scale, data_transform, batch_size):
         running_loss = 0.0
         for batch_number in range(total_batches):
             inputs, labels = give_batch(batch_size, batch_number, dataloader, scale, data_transform) 
+            inputs.to(device)
+            labels.to(device)
             outputs = vgg(inputs)
             loss = criterion(outputs , labels)
             loss.backward()
